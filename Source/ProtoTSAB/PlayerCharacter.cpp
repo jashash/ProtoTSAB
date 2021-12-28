ab8de2e9bbd24964b8256d5a95de185ca9f9caec
jashash <73246088+jashash@users.noreply.github.com>
2021-12-28 14:14:44 -0800

    got animations to work
    idk why but the character doesn't actually MOVE when you press a button

--- Source/ProtoTSAB/PlayerCharacter.cpp
+++ Source/ProtoTSAB/PlayerCharacter.cpp
@@ -15,6 +15,8 @@ APlayerCharacter::APlayerCharacter()
 	GetCapsuleComponent()->InitCapsuleSize(40.0f, 95.0f);
 
 	CharacterMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Character Mesh"));
+	CharacterMesh->AddRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+	CharacterMesh->AddRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
 
 }
 
@@ -30,6 +32,11 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
 }
 
 // Called when the game starts or when spawned
@@ -41,9 +48,21 @@ void APlayerCharacter::BeginPlay()
 
 void APlayerCharacter::MoveForward(float Value)
 {
+	if (Value != 0.0f)
+	{
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
 }
 
 void APlayerCharacter::MoveRight(float Value)
+{
+	if (Value != 0.0f)
+	{
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+
+void APlayerCharacter::TurnAtRate(float Rate)
 {
 }
 
