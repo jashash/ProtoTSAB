5677b813c5b12e50f19646e3dd0abb33743bf78e
jashash <73246088+jashash@users.noreply.github.com>
2021-12-29 16:05:11 -0800

    Character movement works(mostly)
    mouse controls direction of movement and I want it to just be the aim with movement in relation to world

--- Source/ProtoTSAB/PlayerCharacter.h
+++ Source/ProtoTSAB/PlayerCharacter.h
@@ -11,6 +11,16 @@ class PROTOTSAB_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	/*
+	// Camera boom positioning the camera behind the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	// Follow camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* FollowCamera;
+	*/
+
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
@@ -21,6 +31,20 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		float BaseLookUpRate;
+
+	/*
+	// Returns CameraBoom subobject
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	// Returns FollowCamera subobject
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+*/
 
 protected:
 	// Called when the game starts or when spawned
@@ -29,4 +53,5 @@ protected:
 	void MoveForward(float Value);
 	void MoveRight(float Value);
 	void TurnAtRate(float Rate);
+	void LookUpAtRate(float Rate);
 };
